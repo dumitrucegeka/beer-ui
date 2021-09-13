e6fef56ae8ffbd2c7fb79f7111f5fdeb6cfc1702
dumitruc <dumitru.cernei@cegeka.com>
2021-09-13 17:13:44 +0300

    Add eslint. Add jest dependency(no config yet).

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import './App.css';
-import BeerList from './screens/beer-list/BeerList';
-import { BEERS_MOCK } from './beers';
-import Search from './components/Search';
-import { ChangeEvent, useState, useEffect } from 'react';
+import './App.css'
+import BeerList from './screens/beer-list/BeerList'
+import Search from './components/Search'
+import { ChangeEvent, useState, useEffect } from 'react'
 
-function App() {
-  const [currentSearch, setCurrentSearch] = useState('');
-  const [listContent, setListContent] = useState([]);
-  
+function App () {
+  const [currentSearch, setCurrentSearch] = useState('')
+  const [listContent, setListContent] = useState([])
+
   const handleSearch = (event: ChangeEvent) => {
     const searchedValue = (event.target as HTMLInputElement).value
-    console.log({searchedValue})
-    
+    console.log({ searchedValue })
+
     setCurrentSearch(searchedValue)
   }
 
-    useEffect(() => {
-        fetch("https://api.punkapi.com/v2/beers")
-            .then(res => res.json())
-            .then((result) => setListContent(result))
-    });
+  useEffect(() => {
+    fetch('https://api.punkapi.com/v2/beers')
+      .then(res => res.json())
+      .then((result) => setListContent(result))
+  })
 
   return (
     <div className="App">
       <h1>$PLACEHOLDER</h1>
 
       <Search selectedValue={currentSearch} onChange={handleSearch}></Search>
-      
+
       <BeerList beers={listContent}></BeerList>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
