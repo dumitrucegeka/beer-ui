86a3acdabdbf386a4f46c37baea0b49216100982
dumitruc <dumitru.cernei@cegeka.com>
2021-09-16 14:44:10 +0300

    Remove eslint ignore

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,6 @@ import BeerList from './screens/beer-list/BeerList'
 import Search from './components/Search'
 import React, { ChangeEvent, useState, useEffect } from 'react'
 import axios from 'axios'
-// eslint-disable-next-line no-unused-vars
 
 function App () {
   const [currentSearch, setCurrentSearch] = useState('')
@@ -11,32 +10,40 @@ function App () {
 
   const handleSearch = (event: ChangeEvent) => {
     const searchedValue = (event.target as HTMLInputElement).value
-    console.log((event.nativeEvent.target as HTMLInputElement).value);
-    console.log({ searchedValue })
     setCurrentSearch(searchedValue)
   }
 
-  useEffect(() => {
+  function getAllBeers() {
     axios.get('https://api.punkapi.com/v2/beers')
-      .then(res => res.data)
-      .then((result) => setListContent(result))
+        .then(res => res.data)
+        .then((result) => setListContent(result))
+  }
+
+  useEffect(() => {
+    getAllBeers();
   }, [])
+
+  function getBeerByName() {
+    const requestConfig = {params: {beer_name: currentSearch}}
+    console.log({requestConfig})
+
+    axios.get('https://api.punkapi.com/v2/beers', requestConfig)
+        .then(result => result.data)
+        .then(result => setListContent(result))
+  }
 
   useEffect(() => {
     console.log({ currentSearch })
     if (currentSearch) {
-      const requestConfig = { params: { beer_name: currentSearch } }
-      console.log({ requestConfig })
-
-        axios.get('https://api.punkapi.com/v2/beers', requestConfig)
-        .then(result => result.data)
-        .then(result => setListContent(result))
+      getBeerByName();
+    } else {
+      getAllBeers()
     }
   }, [currentSearch])
 
   return (
     <div className="App">
-      <h1>$PLACEHOLDER</h1>
+      <h1>Demo beer search app</h1>
       <Search onChange={handleSearch}/>
       <BeerList beers={listContent}/>
     </div>
