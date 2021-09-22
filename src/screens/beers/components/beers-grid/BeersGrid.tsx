a1684a1b5dcde162f1ad39619055c52fc268ddab
Radu Luca <radu.luca@cegeka.com>
2021-09-22 14:35:41 +0200

    fix grid

--- src/screens/beers/components/beers-grid/BeersGrid.tsx
+++ src/screens/beers/components/beers-grid/BeersGrid.tsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import { DataGrid } from '@mui/x-data-grid'
 import { Beer } from '../../../../models/Beer.interface'
-import useGrid from './useGrid'
+import styles from './BeersGrid.module.css'
+import BEER_COLUMNS from './BEER_COLUMNS'
 
 interface BeersGridProps {
   beers: Beer[]
@@ -9,22 +10,23 @@ interface BeersGridProps {
 
 const BeersGrid = (props: BeersGridProps) => {
   const { beers } = props
-  const { rows, columns } = useGrid(beers)
+  const { beersGridStyle } = styles
 
   return (
     <DataGrid
-      rows={rows}
-      columns={columns}
-      components={
-        {
-          // ColumnMenu: MyCustomColumnMenu,
-        }
-      }
-      componentsProps={{
-        columnMenu: { background: 'red', counter: rows.length },
-      }}
-      pageSize={5}
-      rowsPerPageOptions={[5]}
+      className={beersGridStyle}
+      rows={beers}
+      columns={BEER_COLUMNS}
+      // components={
+      // {
+      // ColumnMenu: MyCustomColumnMenu,
+      //   }
+      // }
+      // componentsProps={{
+      //   columnMenu: { background: 'red', counter: rows.length },
+      // }}
+      pageSize={25}
+      rowsPerPageOptions={[5, 10, 25, 50]}
       checkboxSelection
       disableSelectionOnClick
     />
