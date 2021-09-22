cc2ea0bc6ed8ff5111ffdd0313f3ff80e252a425
dumitruc <dumitru.cernei@cegeka.com>
2021-09-22 15:27:40 +0300

    Configure eslint Configure prettier

--- src/screens/beer-list/components/beer-details/BeerDetails.tsx
+++ src/screens/beer-list/components/beer-details/BeerDetails.tsx
@@ -6,9 +6,8 @@ import mappings from '../../../../models/BeerDetailsKeyMappings';
 import FoodPairing from '../food-pairing/FoodPairing';
 
 const BeerDetails = (props: any) => {
-  const { beerDetailsContainer, beerDetailContainer, beerDetailTitle, beerDetailValue } = styles
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
-  const beer = { ...props?.location?.state?.beer } as Beer
+  const { beerDetailsContainer, beerDetailContainer, beerDetailTitle, beerDetailValue } = styles;
+  const beer = { ...props?.location?.state?.beer } as Beer;
 
   function getBeerDetailDisplayValue(beerDetailKey: string): string {
     const mapping = mappings[beerDetailKey];
@@ -17,28 +16,23 @@ const BeerDetails = (props: any) => {
 
   return (
     <div className={beerDetailsContainer}>
-      <img src={beer.image_url} height="500px" width="200px" alt={beer.name} />
+      <img src={beer.image_url} height='500px' width='200px' alt={beer.name} />
       <div>
-          <FoodPairing {...beer.food_pairing} />
-          {Object.keys(beer).map((beerDetail, index) => {
-            if (typeof beer[beerDetail as keyof Beer] === 'object' || beerDetail === 'image_url') {
-              return
-            }
+        <FoodPairing {...beer.food_pairing} />
+        {Object.keys(beer).map((beerDetail, index) => {
+          if (typeof beer[beerDetail as keyof Beer] === 'object' || beerDetail === 'image_url') {
+            return;
+          }
 
-            // eslint-disable-next-line consistent-return
-            return (
-              <div key={`${index.toString() + beer?.name}`} className={beerDetailContainer}>
-                <p className={beerDetailTitle}>
-                  {getBeerDetailDisplayValue(beerDetail)}
-                </p>
-                <p className={beerDetailValue}>
-                  {beer[beerDetail as keyof Beer]}
-                </p>
-              </div>
-            )
-          })}
+          return (
+            <div key={`${index.toString() + beer?.name}`} className={beerDetailContainer}>
+              <p className={beerDetailTitle}>{getBeerDetailDisplayValue(beerDetail)}</p>
+              <p className={beerDetailValue}>{beer[beerDetail as keyof Beer]}</p>
+            </div>
+          );
+        })}
       </div>
     </div>
-  )
-}
-export default BeerDetails
+  );
+};
+export default BeerDetails;
