287119ebe27da33011a0e10681211d0d8b9ee3a8
dumitruc <dumitru.cernei@cegeka.com>
2021-09-29 14:22:20 +0300

    Add jest config

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+const config = {
+  verbose: true,
+  preset: 'preset-react',
+  moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
+  transformIgnorePatterns: ['node_modules/*'],
+};
+
+module.exports = config;
